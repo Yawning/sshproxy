8a3852a6e720b5ecddc52a0e26896a4fecd48144
Yawning Angel <git@schwanenlied.me>
2013-07-22 15:04:44 -0700

    Swipe the setup_py2exe.py script from obfsproxy

---
+++ setup_py2exe.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import py2exe
+import os
+
+topdir = "py2exe_bundle"
+build_path = os.path.join(topdir, "build")
+dist_path = os.path.join(topdir, "dist")
+
+setup(
+    console=["bin/sshproxy"],
+    zipfile="sshproxy.zip",
+    options={
+        "build": {"build_base": build_path},
+        "py2exe": {
+            "includes": ["twisted", "pyptlib"],
+            "dist_dir": dist_path,
+        }
+    }
+)
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
